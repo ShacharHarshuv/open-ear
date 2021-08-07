147c8d3427ef843cac6112b97d605a0b6dbcbf5b
sharshuv-quotient <shachar.harshuv@gmail.com>
2021-08-07 19:21:29 +0300

    Audio test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import {
+  DataService,
+  Message
+} from '../services/data.service';
+import * as Tone from 'tone';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +11,8 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
+  constructor(private data: DataService) {
+  }
 
   refresh(ev) {
     setTimeout(() => {
@@ -19,4 +24,13 @@ export class HomePage {
     return this.data.getMessages();
   }
 
+  async onPlayClicked(): Promise<void> {
+    await Tone.start();
+    console.log('audio is ready');
+    //create a synth and connect it to the main output (your speakers)
+    const synth = new Tone.Synth().toDestination();
+
+    //play a middle 'C' for the duration of an 8th note
+    synth.triggerAttackRelease('C4', '8n');
+  }
 }
