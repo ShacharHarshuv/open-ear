5a98a4a7170f8025eeab92152e2295426438c147
ShacharHarshuv <shachar.harshuv@gmail.com>
2021-12-17 14:33:11 +0200

    Remove unused message module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-
 import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
-import { MessageComponentModule } from '../message/message.module';
 import { ExerciseSummaryComponent } from './components/exercise-summary/exercise-summary.component';
 
 @NgModule({
@@ -13,7 +11,6 @@ import { ExerciseSummaryComponent } from './components/exercise-summary/exercise
     CommonModule,
     FormsModule,
     IonicModule,
-    MessageComponentModule,
     HomePageRoutingModule,
   ],
   declarations: [
