41252e06b54aed424da34fb55c58b58aa29387dd
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2022-01-08 18:42:17 +0200

    #40/Upgrade angular 13 (#52)
    * Upgrade to ng13
    
    Ran upgrade command (not stabilized build yet)
    
    * Disable typescript lib check
    
    * Add cordova-res as dev dependency
    
    * Merge conflict fix + gitignore layout fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy, } from '@ionic/angular';
-
+import {
+  IonicModule,
+  IonicRouteStrategy,
+} from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
@@ -30,14 +31,13 @@ import { ReleaseNotesModule } from './release-notes/release-notes.module';
   providers: [
     {
       provide: RouteReuseStrategy,
-      useClass: IonicRouteStrategy
+      useClass: IonicRouteStrategy,
     },
     VersionService,
     AppVersion,
   ],
   bootstrap: [AppComponent],
-  exports: [
-  ]
+  exports: [],
 })
 export class AppModule {
 }
