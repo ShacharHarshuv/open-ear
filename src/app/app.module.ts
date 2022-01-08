c9c4dfae662a0970f6d3d8bef5de6aaaa54842dd
sharshuv-quotient <shachar.harshuv@gmail.com>
2022-01-08 18:40:46 +0200

    Merge conflict fix + gitignore layout fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy, } from '@ionic/angular';
 import {
   IonicModule,
   IonicRouteStrategy,
 } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
