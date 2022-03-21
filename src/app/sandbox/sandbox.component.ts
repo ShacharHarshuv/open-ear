b601e1ce67db33a6cd0a20392c67e1b142c79c95
ShacharHarshuv <shachar.harshuv@gmail.com>
2022-03-21 19:07:01 +0200

    Add YouTubeService

--- src/app/sandbox/sandbox.component.ts
+++ src/app/sandbox/sandbox.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import PlayerFactory from 'youtube-player';
+import { YouTubePlayer } from 'youtube-player/dist/types';
+import { BehaviorSubject, interval, NEVER } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import * as PriorityQueue from 'js-priority-queue';
+import { YouTubePlayerService } from '../services/you-tube-player.service';
+
+
 
 @Component({
   selector: 'app-sandbox',
@@ -6,5 +14,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./sandbox.component.scss'],
 })
 export class SandboxComponent {
-  constructor() { }
+
+
+  constructor(
+    public youTubePlayerService: YouTubePlayerService,
+  ) {
+
+    this.youTubePlayerService.addCallback(51, () => {
+      console.log('second chord');
+    });
+    this.youTubePlayerService.addCallback(60, async () => {
+      await this.youTubePlayerService.stop();
+    });
+  }
 }
