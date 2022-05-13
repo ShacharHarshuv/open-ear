0f1e2ebd6703b4c2603e25a997727b87f5b38b90
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2022-05-13 14:02:25 +0300

    Fix isInKey tests (#122)

--- src/app/exercise/utility/music/keys/isInKey.ts
+++ src/app/exercise/utility/music/keys/isInKey.ts
@@ -7,15 +7,17 @@ import {
 } from '../notes/toNoteName';
 import { transpose } from '../transpose';
 import { getDistanceOfKeys } from './getDistanceOfKeys';
+import { mod } from '../../../../shared/ts-utility/mod';
+import { Interval } from '../intervals/Interval';
 
 const CMajorFirstOctave: Note[] =  ['C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1'];
 
 export function isInKey(note: NoteNumberOrName, key: Key) {
   function transposeToFirstOctave(note: NoteNumberOrName): Note {
-    return toNoteName((toNoteNumber(note) - toNoteNumber('C1')) % 12 + toNoteNumber('C1'));
+    return toNoteName(mod(toNoteNumber(note) - toNoteNumber('C1'), Interval.Octave) + toNoteNumber('C1'));
   }
   const noteTransposedToFirstOctave: Note = transposeToFirstOctave(note);
-  const distanceOfKeyFromC = getDistanceOfKeys(key, 'C');
+  const distanceOfKeyFromC: number = mod(getDistanceOfKeys(key, 'C'), Interval.Octave);
   const scaleOfKey: Note[] = transpose(CMajorFirstOctave, distanceOfKeyFromC);
   const scaleOfKeyInFirstOctave = scaleOfKey.map(transposeToFirstOctave);
   return scaleOfKeyInFirstOctave.map(toNoteNumber).includes(toNoteNumber(noteTransposedToFirstOctave));
---
+++ src/app/shared/ts-utility/mod.spec.ts
@@ -0,0 +1,23 @@
+import { mod } from './mod';
+import { testPureFunction } from '../testing-utility/testPureFunction';
+
+describe('mod', () => {
+  testPureFunction(mod, [
+    {
+      args: [5, 12],
+      returnValue: 5,
+    },
+    {
+      args: [14, 12],
+      returnValue: 2,
+    },
+    {
+      args: [-4, 12],
+      returnValue: 8,
+    },
+    {
+      args: [-16, 12],
+      returnValue: 8,
+    }
+  ])
+})
