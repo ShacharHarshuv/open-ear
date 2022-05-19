ca66045928e3189c7a2732921b864dad4921701d
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2022-05-20 00:15:45 +0300

    Scale degrees (#129)

--- src/app/exercise/utility/music/harmony/RomanNumeralChord.spec.ts
+++ src/app/exercise/utility/music/harmony/RomanNumeralChord.spec.ts
@@ -1,47 +1,56 @@
 import {
   RomanNumeralChord,
-  ScaleDegree,
   Accidental,
 } from './RomanNumeralChord';
 import { ChordType } from '../chords';
+import {
+  DiatonicScaleDegree,
+  ScaleDegree,
+} from './ScaleDegrees';
 
 describe('RomanNumeralBuilder', () => {
   const testCases: {
     input: ConstructorParameters<typeof RomanNumeralChord>,
-    degree: ScaleDegree,
+    diatonicDegree: DiatonicScaleDegree,
+    scaleDegree: ScaleDegree,
     accidental?: Accidental,
     type: ChordType,
     serialized: string,
   }[] = [
     {
       input: ['I'],
-      degree: 1,
+      diatonicDegree: 1,
+      scaleDegree: '1',
       type: ChordType.Major,
       serialized: 'I',
     },
     {
       input: ['ii'],
-      degree: 2,
+      diatonicDegree: 2,
+      scaleDegree: '2',
       type: ChordType.Minor,
       serialized: 'ii',
     },
     {
       input: ['bIII'],
-      degree: 3,
+      diatonicDegree: 3,
+      scaleDegree: 'b3',
       accidental: Accidental.Flat,
       type: ChordType.Major,
       serialized: '♭III'
     },
     {
       input: ['#ivdim'],
-      degree: 4,
+      diatonicDegree: 4,
+      scaleDegree: '#4',
       accidental: Accidental.Sharp,
       type: ChordType.Diminished,
       serialized: '♯iv°',
     },
     {
       input: ['viidim'],
-      degree: 7,
+      diatonicDegree: 7,
+      scaleDegree: '7',
       type: ChordType.Diminished,
       serialized: 'vii°',
     },
@@ -50,9 +59,10 @@ describe('RomanNumeralBuilder', () => {
   testCases.forEach(testCase => {
     it(`${JSON.stringify(testCase.input)}`, () => {
       const romanNumeral = new RomanNumeralChord(...testCase.input);
-      expect(romanNumeral.degree).toEqual(testCase.degree);
+      expect(romanNumeral.diatonicDegree).toEqual(testCase.diatonicDegree);
       expect(romanNumeral.accidental).toEqual(testCase.accidental ?? Accidental.Natural);
       expect(romanNumeral.type).toEqual(testCase.type);
+      expect(romanNumeral.scaleDegree).toEqual(testCase.scaleDegree);
       expect(romanNumeral.toString()).toEqual(testCase.serialized);
     });
   })
