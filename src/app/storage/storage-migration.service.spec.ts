ab67baa250064319df2d31454c2e16b4ee5cd798
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2022-05-21 23:43:43 +0300

    getScriptsToRun

--- src/app/storage/storage-migration.service.spec.ts
+++ src/app/storage/storage-migration.service.spec.ts
@@ -2,26 +2,47 @@ import { TestBed } from '@angular/core/testing';
 import {
   StorageMigrationService,
   StorageMigrationScript,
+  MIGRATION_SCRIPTS,
 } from './storage-migration.service';
+import { VersionServiceMock } from '../version.service.mock';
+import { StorageServiceMock } from './storage.service.mock';
+import { StorageService } from './storage.service';
+import Expected = jasmine.Expected;
+import ArrayContaining = jasmine.ArrayContaining;
 
 describe('StorageMigrationService', function() {
+  const baseMockScript: StorageMigrationScript = {
+    breakingChangeVersion: '1.0.0',
+    storageKey: 'key',
+    getNewData(oldData: any): any {
+    },
+  }
   let storageMigrationService: StorageMigrationService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         StorageMigrationService,
+        ...VersionServiceMock.providers,
+        ...StorageServiceMock.providers,
+        {
+          provide: MIGRATION_SCRIPTS,
+          useValue: [],
+        },
       ],
     }).compileComponents();
-
-    storageMigrationService = TestBed.inject(StorageMigrationService);
   });
 
   it('runMigrationScripts', async () => {
+    storageMigrationService = TestBed.inject(StorageMigrationService);
     const migrationScriptsMock: StorageMigrationScript[] = [
       {
+        ...baseMockScript,
+        breakingChangeVersion: '1.1.2',
       },
       {
+        ...baseMockScript,
+        breakingChangeVersion: '1.4.2',
       },
     ];
     spyOn(storageMigrationService, 'getScriptsToRun').and.returnValue(Promise.resolve(migrationScriptsMock));
@@ -31,4 +52,81 @@ describe('StorageMigrationService', function() {
       args: [script],
     })))
   });
+
+  describe('getScriptsToRun', () => {
+    const mockScripts: StorageMigrationScript[] = [
+      {
+        ...baseMockScript,
+        breakingChangeVersion: '1.1.2',
+      },
+      {
+        ...baseMockScript,
+        breakingChangeVersion: '1.1.3',
+      },
+      {
+        ...baseMockScript,
+        breakingChangeVersion: '1.2.0',
+      },
+      {
+        ...baseMockScript,
+        breakingChangeVersion: '1.2.1',
+      },
+    ];
+
+    const testCases: {
+      name: string;
+      currentVersion: string;
+      lastVersionValue: string | undefined;
+      expectedValue: Expected<ArrayLike<StorageMigrationScript>> | ArrayContaining<StorageMigrationScript>;
+    }[] = [
+      {
+        name: 'when lastVersion is not set, should return all scripts',
+        currentVersion: '1.2.3',
+        lastVersionValue: undefined,
+        expectedValue: mockScripts,
+      },
+      {
+        name: 'when lastVersion = last migration script version, should return no script',
+        currentVersion: '1.2.1',
+        lastVersionValue: '1.2.1',
+        expectedValue: [],
+      },
+      {
+        name: 'when lastVersion > last migration script version, should return no script',
+        currentVersion: '1.2.2',
+        lastVersionValue: '1.2.2',
+        expectedValue: [],
+      },
+      {
+        name: 'return all scripts between lastVersionValue and currentVersion',
+        currentVersion: '1.2.1',
+        lastVersionValue: '1.1.3',
+        expectedValue: [
+          jasmine.objectContaining({
+            breakingChangeVersion: '1.2.0',
+          }),
+          jasmine.objectContaining({
+            breakingChangeVersion: '1.2.1',
+          }),
+        ],
+      },
+    ]
+
+    testCases.forEach(testCase => {
+      fit(testCase.name, async () => {
+        TestBed.overrideProvider(MIGRATION_SCRIPTS, {
+          useValue: mockScripts,
+        });
+        storageMigrationService = TestBed.inject(StorageMigrationService);
+        TestBed.inject(VersionServiceMock).version$.next(testCase.currentVersion)
+        spyOn(TestBed.inject(StorageService), 'get').and.callFake((key) => {
+          if (key === 'lastVersion') {
+            return Promise.resolve(testCase.lastVersionValue);
+          }
+          return Promise.resolve();
+        });
+        expect(await storageMigrationService.getScriptsToRun()).toEqual(testCase.expectedValue);
+      });
+    })
+  })
 });
--- src/app/storage/storage-migration.service.ts
+++ src/app/storage/storage-migration.service.ts
@@ -1,16 +1,56 @@
-import { Injectable } from '@angular/core';
+import {
+  Injectable,
+  Inject,
+  InjectionToken,
+} from '@angular/core';
+import {
+  OneOrMany,
+  toObservable,
+} from '../shared/ts-utility';
+import { VersionService } from '../version.service';
+import { StorageService } from './storage.service';
+import { firstValueFrom } from 'rxjs';
+import { versionComparator } from '../release-notes/version-comparator';
 
-export interface StorageMigrationScript {
+export interface StorageMigrationScript<GDataType = any> {
+  breakingChangeVersion: string;
+  storageKey: OneOrMany<string>; // if more than one provided, script will be run for all provided keys
+  getNewData(oldData: GDataType): GDataType;
 }
+
+export const MIGRATION_SCRIPTS = new InjectionToken<StorageMigrationScript[]>('MigrationScrtips');
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageMigrationService {
-  async getScriptsToRun(): Promise<StorageMigrationScript[]> {
-    return [];
+  private readonly _lastVersionKey: string = 'lastVersion';
+
+  constructor(
+    private _versionService: VersionService,
+    private _storageService: StorageService,
+    @Inject(MIGRATION_SCRIPTS) private _migrationScrips: StorageMigrationScript[],
+  ) {
   }
 
+  async getScriptsToRun(): Promise<StorageMigrationScript[]> {
+    const currentVersion: string = await firstValueFrom(toObservable(await this._versionService.version$));
+    const lastVersion: string = await this._storageService.get(this._lastVersionKey);
+    if (!lastVersion) {
+      return this._migrationScrips;
+    }
+    if (lastVersion === currentVersion) {
+      return [];
+    }
+    return this._migrationScrips.filter(migrationScript => {
+      return versionComparator(migrationScript.breakingChangeVersion, lastVersion) > 0 &&
+        versionComparator(migrationScript.breakingChangeVersion, currentVersion) <= 0;
+    });
+  }
+
+  /**
+   * Todo: verify that if key does not exist, nothing is done (it's still not saved)
+   * */
   async runScript(migrationScript: StorageMigrationScript): Promise<void> {
   }
 
