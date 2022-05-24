84ff02bc3dd0c175d2e4fc11ac1af9c0775b1043
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2022-05-24 15:43:05 +0300

    Fix migration script

--- src/app/storage/migration-scripts/storage-migration-1.3.2.spec.ts
+++ src/app/storage/migration-scripts/storage-migration-1.3.2.spec.ts
@@ -5,6 +5,26 @@ describe('storage-migration-1.3.2', () => {
   testPureFunction(migrationScript_1_3_2.getNewData, [
     {
       args: [{
+        exercise1: {},
+      }],
+      returnValue: {
+        exercise1: {},
+      },
+    },
+    {
+      args: [{
+        exercise1: {
+          exerciseSettings: {}
+        },
+      }],
+      returnValue: {
+        exercise1: {
+          exerciseSettings: {}
+        },
+      },
+    },
+    {
+      args: [{
         exercise1: {
           exerciseSettings: {
             includedAnswers: ['♭II', 'i', 'iv', '#iv', 'vii°'],
--- src/app/storage/migration-scripts/storage-migration-1.3.2.ts
+++ src/app/storage/migration-scripts/storage-migration-1.3.2.ts
@@ -1,17 +1,19 @@
 import { StorageMigrationScript } from '../storage-migration.service';
 import * as _ from 'lodash';
 
-export const migrationScript_1_3_2: StorageMigrationScript<Record<string, { exerciseSettings: { includedAnswers: any[] }
-}>> = {
+export const migrationScript_1_3_2: StorageMigrationScript<Record<string, { exerciseSettings?: { includedAnswers?: any[] }}>> = {
   storageKey: 'exerciseSettings',
   breakingChangeVersion: '1.3.2',
   getNewData(oldData) {
     console.log('migrating', oldData);
     const x = _.mapValues(oldData, exerciseSettings => {
+      if (!exerciseSettings.exerciseSettings?.includedAnswers) {
+        return exerciseSettings;
+      }
       return {
         ...exerciseSettings,
         exerciseSettings: {
-          ...exerciseSettings,
+          ...exerciseSettings.exerciseSettings,
           includedAnswers: _.map(exerciseSettings.exerciseSettings.includedAnswers, answer => {
             return answer.replace('♭', 'b').replace('°', 'dim');
           }),
