dc301fa70d0263d809977f702fbdfcf4fb079bfb
Shachar Har-Shuv <shachar.harshuv@gmail.com>
2022-06-08 17:14:59 +0300

    Support voice leading with 4 voices (#152)

--- src/app/exercise/utility/music/chords/voiceChordProgressionWithVoiceLeading.ts
+++ src/app/exercise/utility/music/chords/voiceChordProgressionWithVoiceLeading.ts
@@ -1,7 +1,7 @@
 import { Note } from 'tone/Tone/core/type/NoteUnits';
 import {
   Chord,
-  ChordSymbol
+  ChordSymbol,
 } from './Chord/Chord';
 import { toNoteNumber } from '../notes/toNoteName';
 import * as _ from 'lodash';
@@ -27,13 +27,37 @@ function voiceNextChord(currentChordVoicing: Note[], nextChord: Chord): Note[] {
     voicingOptionsForNextChord.push(possibleVoicing);
   }
 
+  /* Double some voices so the two voicings will have equal number of voices*/
+  function balanceVoicing(voicing1: ReadonlyArray<Note>, voicing2: ReadonlyArray<Note>): [ReadonlyArray<Note>, ReadonlyArray<Note>] {
+    if (voicing1.length === voicing2.length) {
+      return [voicing1, voicing2];
+    } else if (voicing1.length > voicing2.length) {
+      return _.reverse(balanceVoicing(voicing2, voicing1));
+    } else { // voicing1.length < voicing2.length
+      const balancedVoicing1: Note[] = [...voicing1];
+      const balanceVoicing2: Note[] = [...voicing2];
+      while(balancedVoicing1.length != balanceVoicing2.length) {
+        for (let i = 0; i < balancedVoicing1.length; i++) {
+          if (!balanceVoicing2.includes(balancedVoicing1[i])) {
+            balancedVoicing1.splice(i, 0, balancedVoicing1[i]);
+            i++; // advance i to avoid duplicating the same note again
+
+            if (balancedVoicing1.length === balanceVoicing2.length) {
+              break;
+            }
+          }
+        }
+      }
+
+      return [balancedVoicing1, balanceVoicing2];
+    }
+  }
+
   // filter valid voicing (that has small movements in voices)
   const validVoicingOptions: Note[][] = voicingOptionsForNextChord.filter((voicingOption: Note[]): boolean => {
-    if (voicingOption.length !== currentChordVoicing.length) {
-      throw new Error(`voicing of different length not supported`); // (for now)
-    }
-    const rank: number = _.sum(voicingOption.map((voice: Note, index: number): number => {
-      return Math.abs(toNoteNumber(voice) - toNoteNumber(currentChordVoicing[index]));
+    const [_voicingOption, _currentVoicing] = balanceVoicing(voicingOption, currentChordVoicing);
+    const rank: number = _.sum(_voicingOption.map((voice: Note, index: number): number => {
+      return Math.abs(toNoteNumber(voice) - toNoteNumber(_currentVoicing[index]));
     }));
 
     return (rank / voicingOption.length <= MAX_AVG_VOICE_MOVEMENT);
@@ -42,7 +66,7 @@ function voiceNextChord(currentChordVoicing: Note[], nextChord: Chord): Note[] {
   return randomFromList(validVoicingOptions);
 }
 
-export function voiceChordProgressionWithVoiceLeading(chordOrChordSymbolList: (ChordSymbol | Chord)[], startingTopVoicesInversion: number = 0, options: {withBass: boolean} = {withBass: true}): Note[][] {
+export function voiceChordProgressionWithVoiceLeading(chordOrChordSymbolList: (ChordSymbol | Chord)[], startingTopVoicesInversion: number = 0, options: { withBass: boolean } = {withBass: true}): Note[][] {
   const chordList: Chord[] = chordOrChordSymbolList.map((chordOrChordSymbol): Chord => {
     if (chordOrChordSymbol instanceof Chord) {
       return chordOrChordSymbol;
