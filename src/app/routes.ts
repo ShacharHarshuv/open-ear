203c63dd32d778361a0e6f30749563cd6dfd862d
ShacharHarshuv <shachar.harshuv@gmail.com>
2023-09-14 15:29:34 -0400

    Remove AppRouterModule

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,7 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { SandboxComponent } from './sandbox/sandbox.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/routes').then((m) => m.routes),
@@ -25,11 +24,3 @@ const routes: Routes = [
     pathMatch: 'full',
   },
 ];
-
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-  ],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,49 @@
 import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { environment } from './environments/environment';
-import { AppComponent } from './app/app.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { ReleaseNotesModule } from './app/release-notes/release-notes.module';
-import { IonicStorageModule } from '@ionic/storage-angular';
-import { AppRoutingModule } from './app/app-routing.module';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
 import { AppVersion } from '@ionic-native/app-version/ngx';
-import { VersionService } from './app/version.service';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { AppComponent } from './app/app.component';
+import { ReleaseNotesModule } from './app/release-notes/release-notes.module';
+import { routes } from './app/routes';
 import { migrationScripts } from './app/storage/migration-scripts/migration-scripts';
 import { MIGRATION_SCRIPTS } from './app/storage/storage-migration.service';
-import { IonicRouteStrategy, IonicModule } from '@ionic/angular';
-import { RouteReuseStrategy } from '@angular/router';
+import { VersionService } from './app/version.service';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), ReleaseNotesModule, DragDropModule),
-        {
-            provide: RouteReuseStrategy,
-            useClass: IonicRouteStrategy,
-        },
-        {
-            provide: MIGRATION_SCRIPTS,
-            useValue: migrationScripts,
-        },
-        VersionService,
-        AppVersion,
-        provideAnimations(),
-    ]
-})
-  .catch((err) => console.log(err));
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(
+      BrowserModule,
+      IonicModule.forRoot(),
+      IonicStorageModule.forRoot(),
+      ReleaseNotesModule,
+      DragDropModule
+    ),
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy,
+    },
+    {
+      provide: MIGRATION_SCRIPTS,
+      useValue: migrationScripts,
+    },
+    VersionService,
+    AppVersion,
+    provideAnimations(),
+  ],
+}).catch((err) => console.log(err));
