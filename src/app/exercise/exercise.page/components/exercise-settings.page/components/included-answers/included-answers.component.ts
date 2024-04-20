87c0bc5e4d05d0430d29257d018568ea5e472820
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-04-20 17:47:51 -0400

    Allow toggle all answers

--- src/app/exercise/exercise.page/components/exercise-settings.page/components/included-answers/included-answers.component.ts
+++ src/app/exercise/exercise.page/components/exercise-settings.page/components/included-answers/included-answers.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Signal, computed } from '@angular/core';
+import { Component, Signal, computed, input } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { IonicModule } from '@ionic/angular';
-import { signalFromProperty } from '../../../../../../shared/ng-utilities/signalFromProperty';
 import {
   BaseControlValueAccessorComponent,
   getNgValueAccessorProvider,
 } from '../../../../../../shared/ts-utility';
-import Exercise, { isMultiAnswerCell } from '../../../../../exercise-logic';
+import Exercise, {
+  flatAnswerList,
+  isMultiAnswerCell,
+} from '../../../../../exercise-logic';
 import { AnswersLayoutComponent } from '../../../answers-layout/answers-layout.component';
 import { IncludedAnswersButtonComponent } from './components/included-answers-button/included-answers-button.component';
 import { IncludedAnswersMultiAnswerButtonComponent } from './components/included-answers-multi-answer-button/included-answers-multi-answer-button.component';
@@ -29,16 +31,16 @@ import { IncludedAnswersMultiAnswerButtonComponent } from './components/included
 export class IncludedAnswersComponent<
   GAnswer extends string,
 > extends BaseControlValueAccessorComponent<GAnswer[]> {
-  @Input({
-    required: true,
-    alias: 'answerList',
-  })
-  answerListInput: Exercise.AnswerList<GAnswer> = [];
+  readonly answerList = input.required<Exercise.AnswerList<GAnswer>>();
 
-  readonly answerList = signalFromProperty(this, 'answerListInput');
+  readonly allAnswers = computed(() => flatAnswerList(this.answerList()));
 
   readonly includedAnswers: Signal<readonly GAnswer[]> = toSignal(this.value$, {
     initialValue: [],
+  });
+
+  readonly areAllAnswersSelected = computed(() => {
+    return this.allAnswers().length === this.includedAnswers().length;
   });
 
   readonly hasMultiAnswerButtons = computed(() => {
@@ -58,6 +60,10 @@ export class IncludedAnswersComponent<
     } else {
       this.setViewValue([...currentValue, answer]);
     }
+  }
+
+  toggleSelectAll() {
+    this.setViewValue(this.areAllAnswersSelected() ? [] : this.allAnswers());
   }
 }
 
