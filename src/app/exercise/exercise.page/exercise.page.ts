020db12ec4f02e9cacdac6df6b5522a31225a50c
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-07-16 22:25:36 -0400

    Fix answer layout iterator

--- src/app/exercise/exercise.page/exercise.page.ts
+++ src/app/exercise/exercise.page/exercise.page.ts
@@ -87,7 +87,9 @@ export class ExercisePage extends BaseComponent {
       this._rightAnswer.set(answer);
       this._wrongAnswers.set([]);
     } else {
-      this._wrongAnswers.update((wrongAnswers) => [...wrongAnswers, answer]);
+      if (!this._wrongAnswers().includes(answer)) {
+        this._wrongAnswers.update((wrongAnswers) => [...wrongAnswers, answer]);
+      }
     }
     setTimeout(() => {
       if (this.state.globalSettings().revealAnswerAfterFirstMistake) {
