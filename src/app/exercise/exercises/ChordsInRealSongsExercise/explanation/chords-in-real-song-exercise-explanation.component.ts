9d57954736647641fdc35d8216e0ded2ee94b4a9
ShacharHarshuv <shachar.harshuv@gmail.com>
2024-09-28 13:13:20 -0400

    Add explanations for changes

---
+++ src/app/exercise/exercises/ChordsInRealSongsExercise/explanation/chords-in-real-song-exercise-explanation.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chords-in-real-song-exercise-explanation',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      chords-in-real-song-exercise-explanation works!
+    </p>
+  `,
+  styles: ``
+})
+export class ChordsInRealSongExerciseExplanationComponent {
+
+}
--- src/app/exercise/exercises/utility/settings/IncludedAnswersSettings.ts
+++ src/app/exercise/exercises/utility/settings/IncludedAnswersSettings.ts
@@ -1,5 +1,5 @@
 import { toGetter } from '../../../../shared/ts-utility';
-import Exercise from '../../../exercise-logic';
+import Exercise, { isMultiAnswerCell } from '../../../exercise-logic';
 import { CreateExerciseParams } from '../exerciseAttributes/createExercise';
 import { SettingsParams } from './SettingsParams';
 import filterIncludedAnswers = Exercise.filterIncludedAnswers;
@@ -32,26 +32,50 @@ export function includedAnswersSettings<GAnswer extends string>(config?: {
             toGetter(params.answerList)(params.defaultSettings ?? {}),
           ),
       },
-      settingsDescriptors: (settings) => [
-        {
-          key: 'includedAnswers',
-          descriptor: {
-            controlType: 'included-answers',
-            label: 'Included ' + (config?.name ?? 'Options'),
-            answerList: toGetter(params.answerList)(settings),
+      settingsDescriptors: (settings) => {
+        const answerList = toGetter(params.answerList)(settings);
+
+        return [
+          {
+            key: 'includedAnswers',
+            descriptor: {
+              controlType: 'included-answers',
+              label: 'Included ' + (config?.name ?? 'Options'),
+              answerList,
+            },
+            info: (() => {
+              const hasNestedAnswers = (() => {
+                if (Array.isArray(answerList)) {
+                  return false;
+                }
+
+                for (const row of answerList.rows) {
+                  if (Array.isArray(row)) {
+                    for (const answerCell of row) {
+                      if (isMultiAnswerCell(answerCell)) {
+                        return true;
+                      }
+                    }
+                  }
+                }
+
+                return false;
+              })();
+
+              return hasNestedAnswers
+                ? 'Short click on an answer will toggle it. If an answer has a small rectangle at the bottom-right, you can long press it to select more options. When doing the exercise, a single click will select the answer displayed, and a long press and slide will enable you to select inner answers.'
+                : 'Click an answer to include/exclude it from the exercise.';
+            })(),
           },
-        },
-      ],
+        ];
+      },
       answerList: (
         settings: IncludedAnswersSettings<GAnswer>,
       ): AnswerList<GAnswer> => {
-        console.log('included answers', settings.includedAnswers);
-        const includedAnswers = filterIncludedAnswers(
+        return filterIncludedAnswers(
           toGetter(params.answerList)(settings),
           settings.includedAnswers,
         );
-        console.log('included answers', includedAnswers); // todo
-        return includedAnswers;
       },
     };
   };
