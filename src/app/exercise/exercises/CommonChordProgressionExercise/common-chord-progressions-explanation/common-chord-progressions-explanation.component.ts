54dc096956c6099899ad8b4d79c52630eab213fe
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-05-22 17:00:47 -0400

    Fix Chord Function slink

--- src/app/exercise/exercises/CommonChordProgressionExercise/common-chord-progressions-explanation/common-chord-progressions-explanation.component.ts
+++ src/app/exercise/exercises/CommonChordProgressionExercise/common-chord-progressions-explanation/common-chord-progressions-explanation.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 import { InfoPanelComponent } from '../../../../shared/components/shared-components/info-panel/info-panel.component';
 
 @Component({
   selector: 'app-common-chord-progressions-explanation',
   templateUrl: './common-chord-progressions-explanation.component.html',
   standalone: true,
-  imports: [InfoPanelComponent],
+  imports: [InfoPanelComponent, RouterLink],
 })
-export class CommonChordProgressionsExplanationComponent {}
+export class CommonChordProgressionsExplanationComponent {
+  readonly modalController = inject(ModalController);
+}
