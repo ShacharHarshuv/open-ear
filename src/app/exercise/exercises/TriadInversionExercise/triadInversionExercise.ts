85c85f5b2502ef706b39f08cf2e1f703d33f2be4
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-08-09 19:22:52 -0400

    Migrate more exercises (progress)

--- src/app/exercise/exercises/TriadInversionExercise/triadInversionExercise.ts
+++ src/app/exercise/exercises/TriadInversionExercise/triadInversionExercise.ts
@@ -20,11 +20,143 @@ export type TriadInversionExerciseSettings =
       | 'descendingAndAscending';
   };
 
-// export const triadInversionExercise = () => {
-//   const allAnswersList = {
-//     rows: triadInversions.map((triadInversion) => [triadInversion]),
-//   };
+const allAnswersList = {
+  rows: triadInversions.map((triadInversion) => [triadInversion]),
+};
 
+// export const triadInversionExercise: Exercise<
+//   TriadInversionAnswer,
+//   TriadInversionExerciseSettings
+// > = {
+//   id: 'triadInversions',
+//   name: 'Triad Inversions',
+//   summary: 'Find the inversion of a triad in close position',
+//   explanation: TriadInversionExplanationComponent,
+//   answerList: allAnswersList,
+//   logic: (settings) => {
+//     return {
+//       getQuestion(
+//         settings: TriadInversionExerciseSettings,
+//       ): Exclude<Exercise.NotesQuestion<TriadInversionAnswer>, 'cadence'> {
+//         const chordsInC: ChordSymbol[] = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
+//         const randomChordInC: ChordSymbol = randomFromList(chordsInC);
+//         const invertionOptions: TriadPosition[] = [0, 1, 2].filter(
+//           (invertionOption) =>
+//             settings.includedAnswers.includes(triadInversions[invertionOption]),
+//         );
+//         const randomTriadInversion: TriadPosition =
+//           randomFromList(invertionOptions);
+//         const answer = triadInversions[randomTriadInversion];
+//         let voicing: Note[] = new Chord(randomChordInC).getVoicing({
+//           position: randomTriadInversion,
+//           withBass: false,
+//           octave: 3, // picking a lower octave as a high one is more difficult
+//         });
+//         const root: Note = voicing[(3 - randomTriadInversion) % 3];
+//         if (settings.arpeggiateSpeed !== 0) {
+//           switch (settings.arpeggioDirection) {
+//             case 'descending':
+//               voicing = voicing.reverse();
+//               break;
+//             case 'ascendingAndDescending':
+//               voicing = voicing.concat([...voicing].reverse());
+//               break;
+//             case 'descendingAndAscending':
+//               voicing = voicing.reverse().concat([...voicing].reverse());
+//               break;
+//           }
+//         }
+//         const question: Exercise.Question<TriadInversionAnswer> = {
+//           segments: [
+//             {
+//               partToPlay: voicing.map((note, index) => {
+//                 const noteDelay = (index * settings.arpeggiateSpeed) / 100;
+//                 return {
+//                   notes: note,
+//                   velocity: 0.3,
+//                   duration:
+//                     Tone.Time('1n').toSeconds() +
+//                     ((voicing.length - 1) * settings.arpeggiateSpeed) / 100 -
+//                     Tone.Time(noteDelay).toSeconds(),
+//                   time: noteDelay,
+//                 };
+//               }),
+//               rightAnswer: answer,
+//             },
+//           ],
+//           info: '',
+//         };
+
+//         if (settings.playRootAfterAnswer) {
+//           question.afterCorrectAnswer = [
+//             {
+//               partToPlay: toSteadyPart(root, '1n', 0.3),
+//               answerToHighlight: answer,
+//             },
+//           ];
+//         }
+
+//         return question;
+//       },
+//     };
+//   },
+//   settingsDescriptors: [
+//     {
+//       key: 'arpeggiateSpeed',
+//       info:
+//         'When set to a value larger then zero, the chord will be arpeggiated, making it easier to pick up individual notes from it. <br>' +
+//         'Starter with a large settings and gradually reducing can be a good way to train your ear to pick up individual notes being played harmonically',
+//       descriptor: {
+//         controlType: 'slider',
+//         label: 'Arpeggiate Speed',
+//         min: 0,
+//         max: 100,
+//         step: 1,
+//       },
+//     },
+//     {
+//       key: 'playRootAfterAnswer',
+//       descriptor: {
+//         controlType: 'checkbox',
+//         label: 'Play Root After Correct Answer',
+//       },
+//     },
+//     {
+//       key: 'arpeggioDirection',
+//       info:
+//         'Ascending - the chord will be arpeggiated from bottom to top. \n' +
+//         'Descending - the chord will be arpeggiated from top to bottom',
+//       descriptor: {
+//         label: 'Arpeggio Direction',
+//         controlType: 'select',
+//         options: [
+//           {
+//             label: 'Ascending',
+//             value: 'ascending',
+//           },
+//           {
+//             label: 'Descending',
+//             value: 'descending',
+//           },
+//           {
+//             label: 'Ascending & Descending',
+//             value: 'ascendingAndDescending',
+//           },
+//           {
+//             label: 'Descending & Ascending',
+//             value: 'descendingAndAscending',
+//           },
+//         ],
+//       },
+//     },
+//   ],
+//   defaultSettings: {
+//     arpeggiateSpeed: 0,
+//     playRootAfterAnswer: true,
+//   },
+// };
+
+// export const triadInversionExercise = () => {
 //   return composeExercise(
 //     useIncludedAnswers({
 //       name: 'Inversions',
@@ -35,128 +167,5 @@ export type TriadInversionExerciseSettings =
 //       droneSelection: false,
 //     }),
 //     createExercise,
-//   )({
-//     id: 'triadInversions',
-//     name: 'Triad Inversions',
-//     summary: 'Find the inversion of a triad in close position',
-//     explanation: TriadInversionExplanationComponent,
-//     answerList: allAnswersList,
-//     getQuestion(
-//       settings: TriadInversionExerciseSettings,
-//     ): Exclude<Exercise.NotesQuestion<TriadInversionAnswer>, 'cadence'> {
-//       const chordsInC: ChordSymbol[] = ['C', 'Dm', 'Em', 'F', 'G', 'Am'];
-//       const randomChordInC: ChordSymbol = randomFromList(chordsInC);
-//       const invertionOptions: TriadPosition[] = [0, 1, 2].filter(
-//         (invertionOption) =>
-//           settings.includedAnswers.includes(triadInversions[invertionOption]),
-//       );
-//       const randomTriadInversion: TriadPosition =
-//         randomFromList(invertionOptions);
-//       const answer = triadInversions[randomTriadInversion];
-//       let voicing: Note[] = new Chord(randomChordInC).getVoicing({
-//         position: randomTriadInversion,
-//         withBass: false,
-//         octave: 3, // picking a lower octave as a high one is more difficult
-//       });
-//       const root: Note = voicing[(3 - randomTriadInversion) % 3];
-//       if (settings.arpeggiateSpeed !== 0) {
-//         switch (settings.arpeggioDirection) {
-//           case 'descending':
-//             voicing = voicing.reverse();
-//             break;
-//           case 'ascendingAndDescending':
-//             voicing = voicing.concat([...voicing].reverse());
-//             break;
-//           case 'descendingAndAscending':
-//             voicing = voicing.reverse().concat([...voicing].reverse());
-//             break;
-//         }
-//       }
-//       const question: Exercise.Question<TriadInversionAnswer> = {
-//         segments: [
-//           {
-//             partToPlay: voicing.map((note, index) => {
-//               const noteDelay = (index * settings.arpeggiateSpeed) / 100;
-//               return {
-//                 notes: note,
-//                 velocity: 0.3,
-//                 duration:
-//                   Tone.Time('1n').toSeconds() +
-//                   ((voicing.length - 1) * settings.arpeggiateSpeed) / 100 -
-//                   Tone.Time(noteDelay).toSeconds(),
-//                 time: noteDelay,
-//               };
-//             }),
-//             rightAnswer: answer,
-//           },
-//         ],
-//         info: '',
-//       };
-
-//       if (settings.playRootAfterAnswer) {
-//         question.afterCorrectAnswer = [
-//           {
-//             partToPlay: toSteadyPart(root, '1n', 0.3),
-//             answerToHighlight: answer,
-//           },
-//         ];
-//       }
-
-//       return question;
-//     },
-//     settingsDescriptors: [
-//       {
-//         key: 'arpeggiateSpeed',
-//         info:
-//           'When set to a value larger then zero, the chord will be arpeggiated, making it easier to pick up individual notes from it. <br>' +
-//           'Starter with a large settings and gradually reducing can be a good way to train your ear to pick up individual notes being played harmonically',
-//         descriptor: {
-//           controlType: 'slider',
-//           label: 'Arpeggiate Speed',
-//           min: 0,
-//           max: 100,
-//           step: 1,
-//         },
-//       },
-//       {
-//         key: 'playRootAfterAnswer',
-//         descriptor: {
-//           controlType: 'checkbox',
-//           label: 'Play Root After Correct Answer',
-//         },
-//       },
-//       {
-//         key: 'arpeggioDirection',
-//         info:
-//           'Ascending - the chord will be arpeggiated from bottom to top. \n' +
-//           'Descending - the chord will be arpeggiated from top to bottom',
-//         descriptor: {
-//           label: 'Arpeggio Direction',
-//           controlType: 'select',
-//           options: [
-//             {
-//               label: 'Ascending',
-//               value: 'ascending',
-//             },
-//             {
-//               label: 'Descending',
-//               value: 'descending',
-//             },
-//             {
-//               label: 'Ascending & Descending',
-//               value: 'ascendingAndDescending',
-//             },
-//             {
-//               label: 'Descending & Ascending',
-//               value: 'descendingAndAscending',
-//             },
-//           ],
-//         },
-//       },
-//     ],
-//     defaultSettings: {
-//       arpeggiateSpeed: 0,
-//       playRootAfterAnswer: true,
-//     },
-//   });
+//   )();
 // };
