fba1ec4d67115c777b9903b9971124f45989726a
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-08-23 18:19:43 -0400

    Interval exercise stabilize

--- src/app/exercise/exercises/IntervalExercise/intervalExercise.spec.ts
+++ src/app/exercise/exercises/IntervalExercise/intervalExercise.spec.ts
@@ -1,62 +1,58 @@
-// import * as _ from 'lodash';
-// import { ExerciseTest } from '../../ExerciseTest';
-// import Exercise from '../../exercise-logic';
-// import { testExercise } from '../testing-utility/test-exercise.spec';
-// import {
-//   IntervalName,
-//   intervalDescriptorList,
-//   intervalExercise,
-// } from './intervalExercise';
+import * as _ from 'lodash';
+import { ExerciseTest } from '../../ExerciseTest';
+import Exercise from '../../exercise-logic';
+import { toGetter } from '../../utility';
+import { exerciseSmokeTest } from '../testing-utility/test-exercise.spec';
+import {
+  IntervalName,
+  intervalDescriptorList,
+  intervalExercise,
+} from './intervalExercise';
 
-// describe(intervalExercise.name, () => {
-//   const context = testExercise({
-//     getExercise: intervalExercise,
-//     settingDescriptorList: [
-//       'Included Intervals',
-//       'Interval Type',
-//       'Play Wrong Answer',
-//     ],
-//   });
-//   const allIntervals: IntervalName[] = _.map(intervalDescriptorList, 'name');
+describe(intervalExercise.name, () => {
+  exerciseSmokeTest(intervalExercise);
+  const allIntervals = _.map(intervalDescriptorList, 'name');
 
-//   describe('getAnswersList', () => {
-//     it('should include all intervals by default', () => {
-//       expect(context.exercise.getAnswerList()).toEqual(
-//         ExerciseTest.answerListContaining(allIntervals),
-//       );
-//     });
+  describe('getAnswersList', () => {
+    it('should include all intervals by default', () => {
+      expect(
+        toGetter(
+          intervalExercise.logic({
+            ...intervalExercise.settingsConfig.defaults,
+            includedAnswers: allIntervals,
+          }).answerList,
+        )(),
+      ).toEqual(ExerciseTest.answerListContaining(allIntervals));
+    });
 
-//     it('should return only the intervals set by the settings', () => {
-//       const intervals: IntervalName[] = ['Minor 2nd', 'Major 2nd'];
-//       expect(context.exercise.updateSettings).toBeTruthy();
-//       context.exercise.updateSettings?.({
-//         includedAnswers: ['Minor 2nd', 'Major 2nd'],
-//       });
-//       expect(Exercise.flatAnswerList(context.exercise.getAnswerList())).toEqual(
-//         jasmine.arrayWithExactContents(intervals),
-//       );
-//     });
-//   });
+    it('should return only the intervals set by the settings', () => {
+      const intervals: IntervalName[] = ['Minor 2nd', 'Major 2nd'];
+      expect(
+        Exercise.flatAnswerList(
+          toGetter(
+            intervalExercise.logic({
+              ...intervalExercise.settingsConfig.defaults,
+              includedAnswers: ['Minor 2nd', 'Major 2nd'],
+            }).answerList,
+          )(),
+        ),
+      ).toEqual(jasmine.arrayWithExactContents(intervals));
+    });
+  });
 
-//   describe('settings', () => {
-//     it('should have the "included answers" settings', () => {
-//       expect(context.exercise.getSettingsDescriptor?.()).toEqual(
-//         jasmine.arrayContaining([
-//           jasmine.objectContaining<Exercise.SettingsControlDescriptor>({
-//             key: 'includedAnswers',
-//             descriptor: jasmine.objectContaining({
-//               controlType: 'included-answers',
-//               answerList: ExerciseTest.answerListContaining(allIntervals),
-//             }),
-//           }),
-//         ]),
-//       );
-//     });
-//   });
-
-//   describe('getQuestion', () => {
-//     it('should return truthy value', () => {
-//       expect(context.exercise.getQuestion()).toBeTruthy();
-//     });
-//   });
-// });
+  describe('settings', () => {
+    it('should have the "included answers" settings', () => {
+      expect(intervalExercise.settingsConfig.controls).toEqual(
+        jasmine.arrayContaining([
+          jasmine.objectContaining<Exercise.SettingsControlDescriptor>({
+            key: 'includedAnswers',
+            descriptor: jasmine.objectContaining({
+              controlType: 'included-answers',
+              answerList: ExerciseTest.answerListContaining(allIntervals),
+            }),
+          }),
+        ]),
+      );
+    });
+  });
+});
