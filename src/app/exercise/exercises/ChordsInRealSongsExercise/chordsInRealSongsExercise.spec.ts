657732d46fd91ad3e000a59a135afc14cffd2d6a
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-08-23 18:01:52 -0400

    chords in real songs stabilize

--- src/app/exercise/exercises/ChordsInRealSongsExercise/chordsInRealSongsExercise.spec.ts
+++ src/app/exercise/exercises/ChordsInRealSongsExercise/chordsInRealSongsExercise.spec.ts
@@ -1,118 +1,113 @@
-// import * as _ from 'lodash';
-// import { testExercise } from '../testing-utility/test-exercise.spec';
-// import { chordsInRealSongsExercise } from './chordsInRealSongsExercise';
-// import { songChordQuestions } from './songQuestions';
+import * as _ from 'lodash';
+import { exerciseSmokeTest } from '../testing-utility/test-exercise.spec';
+import { chordsInRealSongsExercise } from './chordsInRealSongsExercise';
+import { songChordQuestions } from './songQuestions';
 
-// describe(chordsInRealSongsExercise.name, () => {
-//   const context = testExercise({
-//     getExercise: chordsInRealSongsExercise,
-//     settingDescriptorList: [
-//       'Analyze By',
-//       'Simplify Extensions',
-//       'Included Chords',
-//     ],
-//   });
+describe(chordsInRealSongsExercise.name, () => {
+  exerciseSmokeTest(chordsInRealSongsExercise);
 
-//   describe('Songs', () => {
-//     songChordQuestions.forEach((chordProgressionDescriptor) => {
-//       it(chordProgressionDescriptor.name ?? 'Anonymous Song', () => {
-//         const exercise = chordsInRealSongsExercise();
-//         exercise.updateSettings?.({
-//           tonicForAnalyzing: 'original',
-//           includedChords: _.map(chordProgressionDescriptor.chords, 'chord'),
-//           acceptEquivalentChord: false,
-//           learnProgressions: false,
-//         });
-//         expect(exercise.getQuestion()).toBeTruthy();
-//       });
-//     });
-//   });
+  describe('Songs', () => {
+    songChordQuestions.forEach((chordProgressionDescriptor) => {
+      it(chordProgressionDescriptor.name ?? 'Anonymous Song', () => {
+        expect(
+          chordsInRealSongsExercise
+            .logic({
+              tonicForAnalyzing: 'original',
+              includedChords: _.map(chordProgressionDescriptor.chords, 'chord'),
+              acceptEquivalentChord: false,
+              learnProgressions: false,
+            })
+            .getQuestion(),
+        ).toBeTruthy();
+      });
+    });
+  });
 
-//   // describe('included progressions', () => {
-//   //   let exercise: ReturnType<typeof chordsInRealSongsExercise>;
+  // describe('included progressions', () => {
+  //   let exercise: ReturnType<typeof chordsInRealSongsExercise>;
 
-//   //   function generateMockProgressionDescriptor(
-//   //     progression: RomanNumeralChordSymbol[],
-//   //   ): YouTubeSongQuestion {
-//   //     return {
-//   //       videoId: '',
-//   //       chords: progression.map((chord, index) => ({
-//   //         chord,
-//   //         seconds: index,
-//   //       })),
-//   //       endSeconds: progression.length,
-//   //       mode: Mode.Major,
-//   //       key: 'C',
-//   //     };
-//   //   }
+  //   function generateMockProgressionDescriptor(
+  //     progression: RomanNumeralChordSymbol[],
+  //   ): YouTubeSongQuestion {
+  //     return {
+  //       videoId: '',
+  //       chords: progression.map((chord, index) => ({
+  //         chord,
+  //         seconds: index,
+  //       })),
+  //       endSeconds: progression.length,
+  //       mode: Mode.Major,
+  //       key: 'C',
+  //     };
+  //   }
 
-//   //   const progression_I_IV_V = generateMockProgressionDescriptor([
-//   //     'I',
-//   //     'IV',
-//   //     'V',
-//   //   ]);
-//   //   const progression_I_V_vi_IV = generateMockProgressionDescriptor([
-//   //     'I',
-//   //     'V',
-//   //     'vi',
-//   //     'IV',
-//   //   ]);
-//   //   const progression_I_ii_V_I = generateMockProgressionDescriptor([
-//   //     'I',
-//   //     'ii',
-//   //     'V',
-//   //     'I',
-//   //   ]);
-//   //   const progression_i_bVII_bVI = generateMockProgressionDescriptor([
-//   //     'i',
-//   //     'bVII',
-//   //     'bVI',
-//   //   ]);
+  //   const progression_I_IV_V = generateMockProgressionDescriptor([
+  //     'I',
+  //     'IV',
+  //     'V',
+  //   ]);
+  //   const progression_I_V_vi_IV = generateMockProgressionDescriptor([
+  //     'I',
+  //     'V',
+  //     'vi',
+  //     'IV',
+  //   ]);
+  //   const progression_I_ii_V_I = generateMockProgressionDescriptor([
+  //     'I',
+  //     'ii',
+  //     'V',
+  //     'I',
+  //   ]);
+  //   const progression_i_bVII_bVI = generateMockProgressionDescriptor([
+  //     'i',
+  //     'bVII',
+  //     'bVI',
+  //   ]);
 
-//   //   beforeEach(() => {
-//   //     exercise = chordsInRealSongsExercise();
-//   //   });
+  //   beforeEach(() => {
+  //     exercise = chordsInRealSongsExercise();
+  //   });
 
-//   //   testPureFunction(
-//   //     (
-//   //       selected: RomanNumeralChordSymbol[],
-//   //     ): DeepReadonly<YouTubeSongQuestion[]> => {
-//   //       exercise.updateSettings?.({
-//   //         ...exercise.getCurrentSettings?.(),
-//   //         tonicForAnalyzing: 'major',
-//   //         includedChords: selected,
-//   //         acceptEquivalentChord: false,
-//   //         learnProgressions: false,
-//   //       });
-//   //       return exercise.getAvailableProgressions(
-//   //         exercise.getCurrentSettings?.()!,
-//   //       );
-//   //     },
-//   //     [
-//   //       {
-//   //         args: [['I', 'IV', 'V']],
-//   //         returnValue: jasmine.arrayWithExactContents([progression_I_IV_V]),
-//   //       },
-//   //       {
-//   //         args: [['I', 'IV', 'V', 'vi']],
-//   //         returnValue: jasmine.arrayWithExactContents([
-//   //           progression_I_IV_V,
-//   //           progression_I_V_vi_IV,
-//   //         ]),
-//   //       },
-//   //       {
-//   //         args: [['I', 'ii', 'IV', 'V', 'vi']],
-//   //         returnValue: jasmine.arrayWithExactContents([
-//   //           progression_I_IV_V,
-//   //           progression_I_V_vi_IV,
-//   //           progression_I_ii_V_I,
-//   //         ]),
-//   //       },
-//   //       {
-//   //         args: [['i', 'bVII', 'bVI']],
-//   //         returnValue: jasmine.arrayWithExactContents([progression_i_bVII_bVI]),
-//   //       },
-//   //     ],
-//   //   );
-//   // });
-// });
+  //   testPureFunction(
+  //     (
+  //       selected: RomanNumeralChordSymbol[],
+  //     ): DeepReadonly<YouTubeSongQuestion[]> => {
+  //       exercise.updateSettings?.({
+  //         ...exercise.getCurrentSettings?.(),
+  //         tonicForAnalyzing: 'major',
+  //         includedChords: selected,
+  //         acceptEquivalentChord: false,
+  //         learnProgressions: false,
+  //       });
+  //       return exercise.getAvailableProgressions(
+  //         exercise.getCurrentSettings?.()!,
+  //       );
+  //     },
+  //     [
+  //       {
+  //         args: [['I', 'IV', 'V']],
+  //         returnValue: jasmine.arrayWithExactContents([progression_I_IV_V]),
+  //       },
+  //       {
+  //         args: [['I', 'IV', 'V', 'vi']],
+  //         returnValue: jasmine.arrayWithExactContents([
+  //           progression_I_IV_V,
+  //           progression_I_V_vi_IV,
+  //         ]),
+  //       },
+  //       {
+  //         args: [['I', 'ii', 'IV', 'V', 'vi']],
+  //         returnValue: jasmine.arrayWithExactContents([
+  //           progression_I_IV_V,
+  //           progression_I_V_vi_IV,
+  //           progression_I_ii_V_I,
+  //         ]),
+  //       },
+  //       {
+  //         args: [['i', 'bVII', 'bVI']],
+  //         returnValue: jasmine.arrayWithExactContents([progression_i_bVII_bVI]),
+  //       },
+  //     ],
+  //   );
+  // });
+});
