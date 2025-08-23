1bd733c4cc1f851a78afc212e5e947c8a7d2bbe7
ShacharHarshuv <shachar.harshuv@gmail.com>
2025-08-23 18:23:52 -0400

    notesWithChordExercise.spec

--- src/app/exercise/exercises/NotesWithChords/notesWithChordsExercise.spec.ts
+++ src/app/exercise/exercises/NotesWithChords/notesWithChordsExercise.spec.ts
@@ -1,14 +1,6 @@
-// import { testExercise } from '../testing-utility/test-exercise.spec';
+import { exerciseSmokeTest } from '../testing-utility/test-exercise.spec';
+import { notesWithChordsExercise } from './notesWithChordsExercise';
 
-// describe(notesWithChordsExercise.name, () => {
-//   testExercise({
-//     getExercise: notesWithChordsExercise,
-//     settingDescriptorList: [
-//       'Included Options',
-//       ...expectedKeySelectionSettingsDescriptors,
-//       'Drone',
-//       'Voice Mode',
-//       'Harmony Mode',
-//     ],
-//   });
-// });
+describe(notesWithChordsExercise.name, () => {
+  exerciseSmokeTest(notesWithChordsExercise);
+});
